319ad02ff9ee6fb5c2d847c76fe1adfb3e480759
Thiago Rocha Lira <rochalirathiago@outlook.com>
2023-10-21 14:28:13 -0300

    Criação dos models

---
+++ PegadaChave/Models/Carrinho.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PegadaChave.Models
+{
+    internal class Carrinho
+    {
+        [Key]
+        [Required]
+        public int id_carrinho { get; set; }
+
+        [Required(ErrorMessage = "O ID do cliente é obrigatório.")]
+        public int id_cliente { get; set; }
+
+        [Required(ErrorMessage = "O total do carrinho é obrigatório.")]
+        public float total_carrinho { get; set; }
+
+        [ForeignKey("id_cliente")]
+        public Cliente Cliente { get; set; }
+    }
+}
---
+++ PegadaChave/Models/LookProduto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PegadaChave.Models
+{
+    internal class LookProduto
+    {
+        [Key]
+        [Required]
+        public int id_look { get; set; }
+
+        [Key]
+        [Required]
+        public int id_produto { get; set; }
+
+        [ForeignKey("id_look")]
+        public Look Look { get; set; }
+
+        [ForeignKey("id_produto")]
+        public Produto Produto { get; set; }
+    }
+}
