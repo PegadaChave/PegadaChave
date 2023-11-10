cc7265cf4704e1b4f5eb303b946848f8f0bf97d6
Thiago Rocha Lira <rochalirathiago@outlook.com>
2023-11-10 18:22:55 -0300

    Adicionando lógica ao botão ordenar

--- PegadaChave/Controllers/ProductsController.cs
+++ PegadaChave/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using PegadaChave.Data;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,9 +13,28 @@ namespace PegadaChave.Controllers
         private ProdutoCRUD produtoCRUD = new ProdutoCRUD();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
             var produtos = produtoCRUD.Read();
+
+            switch (sortOrder)
+            {
+                case "preco_asc":
+                    produtos = produtos.OrderByDescending(p => p.PrecoProduto).ToList();
+                    sortOrder = "preco_desc";
+                    break;
+                case "preco_desc":
+                    produtos = produtos.OrderBy(p => p.PrecoProduto).ToList();
+                    sortOrder = "preco_asc";
+                    break;
+                default:
+                    produtos = produtos.OrderBy(p => p.NomeProduto).ToList();
+                    sortOrder = "preco_asc";
+                    break;
+            }
+
+            ViewBag.SortOrder = sortOrder;
+
             return View(produtos);
         }
     }
